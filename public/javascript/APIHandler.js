3add1b662e5526b725a09d459c479d23872531ed
betilla x <aclara_77@yahoo.com>
2022-02-06 14:48:11 +0000

    remove wrong db

--- public/javascript/APIHandler.js
+++ public/javascript/APIHandler.js
@@ -4,22 +4,52 @@ class APIHandler {
   }
 
   getFullList () {
-
+       axios.get(this.BASE_URL+"/characters")
+       .then((data)=>{
+         console.log(data);
+       })
+       .catch((error)=>{
+         console.log(error);
+       })
   }
 
-  getOneRegister () {
-
+  getOneRegister (id) {
+    axios.get(this.BASE_URL+"/characters/:id")
+    .then((data)=>{
+      console.log(data);
+    })
+    .catch((error)=>{
+      console.log(error);
+    })
   }
 
-  createOneRegister () {
-
+  createOneRegister (newInfo) {
+     axios.post(this.BASE_URL+"/characters", newInfo)
+     .then((data)=>{
+      console.log(data);
+    })
+    .catch((error)=>{
+      console.log(error);
+    })
   }
 
-  updateOneRegister () {
-
+  updateOneRegister (id,newInfo) {
+    axios.put(this.BASE_URL+"/characters/:id", newInfo)
+    .then((data)=>{
+      console.log(data);
+    })
+    .catch((error)=>{
+      console.log(error);
+    })
   }
 
-  deleteOneRegister () {
-
+  deleteOneRegister (id) {
+    axios.delete(this.BASE_URL+"/characters/:id")
+    .then((data)=>{
+      console.log(data);
+    })
+    .catch((error)=>{
+      console.log(error);
+    })
   }
 }
